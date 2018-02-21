7e6e55178f901ed6e61431eb449e828f8dceab57
John Santhosh <jsanthos@asu.edu>
2018-02-21 15:43:00 -0700

    Changes for updating screen content.

--- src/main/java/controller/ScreenController.java
+++ src/main/java/controller/ScreenController.java
@@ -1,23 +1,27 @@
 package controller;
 
 
+import model.ScreenModel;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.*;
+
 
 @CrossOrigin("http://localhost:4200")
 @RestController()
 @RequestMapping(value = "/screen-service")
 public class ScreenController {
-    static List<String> screens;
+    List<ScreenModel> screens = null;
 
-    ScreenController() {
-        screens = Stream.of("Screen1", "Screen2").collect(Collectors.toList());
+    void init(HttpSession session) {
+        ScreenModel screen1 = new ScreenModel("1", "Screen1");
+        ScreenModel screen2 = new ScreenModel("2", "Screen1");
+
+        screens = Arrays.asList(screen1, screen2);
+        session.setAttribute("screens", screens);
     }
 
     @CrossOrigin("http://localhost:4200")
@@ -28,20 +32,49 @@ public class ScreenController {
 
     @CrossOrigin("http://localhost:4200")
     @RequestMapping(method = RequestMethod.GET, value = "/screens")
-    List<String> getScreens() {
+    List<ScreenModel> getScreens(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if(CollectionUtils.isEmpty(screens)){
+            init(session);
+        }
+
+        //screens = (List<String>) session.getAttribute("screens");
         return screens;
     }
 
     @CrossOrigin("http://localhost:4200")
     @RequestMapping(method = RequestMethod.GET, value = "/screens/{id}")
-    List<String> getScreen(@PathVariable int id) {
+    List<ScreenModel> getScreen(@PathVariable int id, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if(CollectionUtils.isEmpty(screens)){
+            init(session);
+        }
+
+        //screens = (List<String>) session.getAttribute("screens");
+
         if (!CollectionUtils.isEmpty(screens)) {
             if (id > 0 && id <= screens.size()) {
                 return Arrays.asList(screens.get(id - 1));
             }
         }
 
-        return Arrays.asList("");
+        return Arrays.asList();
+    }
+
+    @CrossOrigin("http://localhost:4200")
+    @RequestMapping(method = RequestMethod.POST, value = "/screens")
+    void updateScreen(@RequestBody Map<String, String> screen) {
+        System.out.println(screen.keySet());
+        String id = screen.get("id");
+        String content = screen.get("content");
+        System.out.println(id + content);
+        int idNum = Integer.parseInt(id);
+
+        if (!CollectionUtils.isEmpty(screens)) {
+            if (idNum > 0 && idNum <= screens.size()) {
+                screens.get(idNum - 1).setContent(content);
+            }
+        }
     }
 
 }
---
+++ src/main/java/model/ScreenModel.java
@@ -0,0 +1,31 @@
+package model;
+
+public class ScreenModel {
+    String id;
+    String content;
+
+    public ScreenModel(){
+
+    }
+
+    public ScreenModel(String id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
